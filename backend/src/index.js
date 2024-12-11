de466583656088b44f0b179b50e83016406cffa8
Ritik Kaintura <ritik.kaintura007@gmail.com>
2024-12-11 18:53:50 +0530

    Project-Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,15 @@
 import express from "express";
-import authRoutes from './routes/auth.route.js'
-import dotenv from "dotenv"
-import path from "path";
-import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
-import messageRoutes from './routes/message.route.js'
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./lib/socket.js";
-import { Server } from "socket.io";
 
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config()
 
@@ -27,7 +28,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/chat-app/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/Chat-app/dist")));
   
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../frontend/Chat-app", "dist", "index.html"));
